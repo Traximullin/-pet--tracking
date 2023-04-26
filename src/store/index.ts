6b59e5321cd510e20cbc4deffb128d68e253daad
Traximullin <66427615+Traximullin@users.noreply.github.com>
2023-04-26 20:37:00 +0300

    persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,42 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER
+} from "redux-persist"
+import storage from "redux-persist/lib/storage"
 import trackerReducer from "./features/trackerSlice"
 import modalReducer from "./features/modalSlice"
 
-export const store = configureStore({
-    reducer: {
-        tracker: trackerReducer,
-        modal: modalReducer,
-    },
+const rootReducer = combineReducers({
+    tracker: trackerReducer,
+    modal: modalReducer,
 })
+
+const persistConfig = {
+    key: "root",
+    storage,
+    whitelist: ["tracker"],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+})
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 
